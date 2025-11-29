8f16159828b0edc3b2ef65be844f620a15ce0147
AmeleworkMurti <amelemurti2@gmail.com>
2025-11-29 02:30:35 -0500

    API key accessed and added

--- App.js
+++ App.js
@@ -13,3 +13,8 @@ import {
 } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+
+const Stack = createNativeStackNavigator();
+
+const API_KEY = "fca_live_1wlibatIllp4HFv7Wq6CCCXAzCuEq113mgaFVe3i";
