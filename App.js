7fe03c26312d0625c1ae1e72b4f02b75c905d251
AmeleworkMurti <amelemurti2@gmail.com>
2025-11-29 02:27:24 -0500

    Imports

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  ActivityIndicator,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
